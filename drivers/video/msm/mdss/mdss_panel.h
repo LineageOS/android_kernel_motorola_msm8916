79fe16c3f4dc2b0317f156759f099273fee16610
Zac Crosby <zac@motorola.com>
2015-09-28 14:16:42 -0500

    msm: mdss: Send FB notification in the middle of display off
    For the Lux LG display, touch is required to send its 'deep sleep'
    commands after display has entered sleep, but before the reset line
    is toggled.
    
    To support this requirement, add a new FB notification to be sent in the
    middle of turning the display off.  This new notification is only sent on
    panels which have the feature enabled.
    
    Change-Id: I16c374d2fae4da263688ca838a2749f917a2bde5
    Signed-off-by: Zac Crosby <zac@motorola.com>
    (cherry picked from commit 492a4ec90d20415b74b294c480215a52371e480c)
    Reviewed-on: http://gerrit.mot.com/789498
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Shi-Yong Li <a22381@motorola.com>
    Reviewed-by: Joseph Swantek <jswantek@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/video/msm/mdss/mdss_panel.h
+++ drivers/video/msm/mdss/mdss_panel.h
@@ -452,6 +452,7 @@ struct mdss_panel_info {
 
 	/* debugfs structure for the panel */
 	struct mdss_panel_debugfs_info *debugfs_info;
+	bool blank_progress_notify_enabled;
 };
 
 struct mdss_panel_data {
@@ -474,6 +475,7 @@ struct mdss_panel_data {
 	int (*event_handler) (struct mdss_panel_data *pdata, int e, void *arg);
 
 	struct mdss_panel_data *next;
+	struct msm_fb_data_type *mfd;
 };
 
 struct mdss_panel_debugfs_info {
