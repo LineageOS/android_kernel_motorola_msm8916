f070c5b418245a60f2dd5a870c003bff9e79b3bc
hritik <hritikdrocks@gmail.com>
2017-08-11 01:58:12 +0000

    Build_script for merlin

---
+++ build_merlin.sh
@@ -0,0 +1,79 @@
+#
+# Custom build script
+#
+# This software is licensed under the terms of the GNU General Public
+# License version 2, as published by the Free Software Foundation, and
+# may be copied, distributed, and modified under those terms.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# Please maintain this if you use this script or any part of it
+#
+
+#!/bin/bash
+KERNEL_DIR=~/HKernel
+KERN_IMG=$KERNEL_DIR/arch/arm/boot/zImage
+DTBTOOL=$KERNEL_DIR/tools/dtbToolCM
+BUILD_START=$(date +"%s")
+blue='\033[0;34m'
+cyan='\033[0;36m'
+yellow='\033[0;33m'
+red='\033[0;31m'
+nocol='\033[0m'
+export ARCH=arm
+export SUBARCH=arm
+export CROSS_COMPILE=../arm-eabi-6.0/bin/arm-eabi-
+export KBUILD_BUILD_USER="hk"
+export KBUILD_BUILD_HOST="jarvis"
+rm -f arch/arm/boot/dts/*.dtb
+rm -f arch/arm/boot/dt.img
+rm -f ../AnyKernel/zImage
+
+compile_kernel ()
+{
+  echo -e "$cyan***********************************************"
+  echo -e "          Initializing defconfig          "
+  echo -e "***********************************************$nocol"
+  make merlin_defconfig
+  echo -e "$cyan***********************************************"
+  echo -e "             Building kernel          "
+  echo -e "***********************************************$nocol"
+  make -j4
+  if ! [ -a $KERN_IMG ];
+  then
+    echo -e "$red Kernel Compilation failed! Fix the errors! $nocol"
+    exit 1
+  fi
+  echo -e "$cyan***********************************************"
+  echo -e "         	Building modules          "
+  echo -e "***********************************************$nocol"
+  make -j4 modules
+}
+
+HKernel ()
+{
+  echo -e "$cyan***********************************************"
+  echo "          Compiling HKernel kernel          "
+  echo -e "***********************************************$nocol"
+  
+     make clean
+     make mrproper
+     compile_kernel
+
+echo -e "$cyan***********************************************"
+echo -e " Converting the output into a flashable zip"
+echo -e "***********************************************$nocol"
+
+find -name '*.ko' -type f -exec cp '{}' ../AnyKernel/modules/ \;
+cp arch/arm/boot/zImage ../AnyKernel/
+cd ~/AnyKernel/
+zip -r9 ../HKernel-v.zip ../AnyKernel/
+today=$(date +"-%d%m%Y")
+}
+HKernel
+BUILD_END=$(date +"%s")
+DIFF=$(($BUILD_END - $BUILD_START))
+echo -e "$yellow Build completed in $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds.$nocol"
