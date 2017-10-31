ca9ad19e1d52963c118fc2435857be04a9545e59
Marissa Wall <marissaw@google.com>
2017-10-31 15:05:13 -0700

    ANDROID: cpufreq: concurrent_active_time by pid
    In order to model the energy used by the cpu,
    we need to expose cpu time information to
    userspace. Output the time each pid spent running
    with 1 to num_possible_cpus online and not idle.
    
    We can use this information to blame pids and
    uids for the energy they are using.
    
    For instance, if 4 cores are online and active
    for 50ms and the pid is running on one of the
    cores, the pid should be blamed for 1/4 of the
    base energy used by the cpu when 1 or more cores is
    active. Using other proc files, the pid can
    also be blamed for the cost of keeping a cluster
    on and the cost of the individual cores it
    is using.
    
    b/68399339
    
    Test: cat /proc/<PID>/concurrent_active_time
    
    Change-Id: Iaecf4ca71e7043a9c7b97787000a74a514eb9f04
    Signed-off-by: Marissa Wall <marissaw@google.com>

--- fs/proc/base.c
+++ fs/proc/base.c
@@ -2783,6 +2783,7 @@ static const struct pid_entry tgid_base_stuff[] = {
 #endif
 #ifdef CONFIG_CPU_FREQ_STAT
 	ONE("time_in_state", 0444, proc_time_in_state_show),
+	ONE("concurrent_active_time", 0444, proc_concurrent_active_time_show),
 #endif
 };
 
@@ -3176,6 +3177,7 @@ static const struct pid_entry tid_base_stuff[] = {
 #endif
 #ifdef CONFIG_CPU_FREQ_STAT
 	ONE("time_in_state", 0444, proc_time_in_state_show),
+	ONE("concurrent_active_time", 0444, proc_concurrent_active_time_show),
 #endif
 };
 
