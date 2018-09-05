f97dd6ee0b6c51feb7b0d97abcde8cd2a4ac91ac
andip71 <andreasp@gmx.de>
2018-09-05 22:45:33 +0900

    mdss_fb: add lcd backlight min limit
    Signed-off-by: andip71 <andreasp@gmx.de>
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>
    Signed-off-by: sunilpaulmathew <sunil.kde@gmail.com>

--- drivers/video/msm/mdss/mdss_fb.c
+++ drivers/video/msm/mdss/mdss_fb.c
@@ -61,6 +61,9 @@
 #define MDSS_FB_NUM 2
 #endif
 
+int backlight_min = 0;
+module_param(backlight_min, int, 0644);
+
 #ifndef EXPORT_COMPAT
 #define EXPORT_COMPAT(x)
 #endif
@@ -270,6 +273,10 @@ static void mdss_fb_set_bl_brightness(struct led_classdev *led_cdev,
 
 	if (value > mfd->panel_info->brightness_max)
 		value = mfd->panel_info->brightness_max;
+
+	// Apply min limits for LCD backlight (0 is exception for display off)
+	if (value != 0 && value < backlight_min)
+		value = backlight_min;
 
 	/* This maps android backlight level 0 to 255 into
 	   driver backlight level 0 to bl_max with rounding */
